6d12ee8d783a1c3e443dae824fcaa537fd80edba
AviraLS22 <aviralsharma.sharma2205@gmail.com>
2025-01-02 23:28:07 +0530

    error resolved in scorer

--- evals/scorers.ts
+++ evals/scorers.ts
@@ -5,16 +5,16 @@ export const ToolCallMatch: Scorer<any, {}> = async ({          // a scorer func
   output,
   expected,
 }: {
-  input: any;
+  input?: any;
   output: { role: string; tool_calls: { function?: { name: string } }[] };
-  expected: { tool_calls: { function?: { name: string } }[] };
+  expected?: { tool_calls: { function?: { name: string } }[] };
 }) => {
   const score =                                                 // if the role of the agent is assistant and the tool call made by the agent is the same as the expected tool call then return 1 else return 0
     output.role === 'assistant' &&
     Array.isArray(output.tool_calls) &&
     output.tool_calls.length === 1 &&
     output.tool_calls[0].function?.name ===
-      expected.tool_calls[0].function?.name
+      expected?.tool_calls?.[0]?.function?.name
       ? 1
       : 0
 
